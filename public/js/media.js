81b63aa626209897d45ac524f19a06880322c92d
Matthieu Leorat <matthieu.leorat@gmail.com>
2018-02-06 16:02:04 +0100

    Add assets

---
+++ public/js/media.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function($) {
+    var $body = $('body');
+
+    // on popup opening
+    $body.on('click', '.media-popin-with-tabs', function(event) {
+
+        // List Tabs Container
+        var $tabsListContainer = '.js-tablist';
+
+        // Find opened tab
+        var $selectedTab = $body.find($tabsListContainer + ' a[aria-selected=true]');
+
+        // Find tab content container
+        var $tabContainer = $('#'+$($selectedTab).attr('aria-controls'));
+
+        // Where to find content to load
+        var url = $($selectedTab).attr('data-url');
+
+        $.ajax({
+            type: 'POST',
+            url: url
+        }).success(function(data) {
+            $($tabContainer).html(data);
+        }).error(function(xhr, ajaxOptions, thrownError) {
+            $($tabContainer).html(xhr.responseText);
+        });
+    });
+
+
+    // On tab click
+    $body.on('click', '.js-tablist__link', function(event) {
+        // Where to find content to load
+        var url = $(this).attr('data-url');
+
+        // Find tab content container
+        var $tabContainer = $('#'+$(this).attr('aria-controls'));
+
+        $.ajax({
+            type: 'POST',
+            url: url
+        }).success(function(data) {
+            $($tabContainer).html(data);
+        }).error(function(xhr, ajaxOptions, thrownError) {
+            $($tabContainer).html(xhr.responseText);
+        });
+    });
+});
