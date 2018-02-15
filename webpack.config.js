9a6bc680792097546ca753dbcda047707da024cf
Matthieu Leorat <matthieu.leorat@gmail.com>
2018-02-15 12:03:09 +0100

    try specify yarn version

--- webpack.config.js
+++ webpack.config.js
@@ -20,17 +20,13 @@ Encore
 
     // uncomment for legacy applications that require $/jQuery as a global variable
     .autoProvidejQuery()
-
     .enableSourceMaps(!Encore.isProduction())
 
     // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
 
     // show OS notifications when builds finish/fail
-    // .enableBuildNotifications()
-
-    // create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning()
+    .enableBuildNotifications()
 ;
 
 module.exports = Encore.getWebpackConfig();
