1528b033c5a009c0a8c625f31b28b21c3a00b49f
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-03-02 11:15:13 +0100

    Add headImage in Post

--- src/Entity/Editorial/Post.php
+++ src/Entity/Editorial/Post.php
@@ -10,6 +10,7 @@ namespace App\Entity\Editorial;
 
 use App\Behavior\Statusable;
 use App\Behavior\Titlable;
+use App\Entity\Media\Image;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
 use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
@@ -34,6 +35,30 @@ class Post {
 	 * @ORM\GeneratedValue(strategy="AUTO")
 	 */
 	protected $id;
+	/**
+	 * @var Image
+	 * @ORM\ManyToOne(targetEntity="App\Entity\Media\Image")
+	 * @ORM\JoinColumn(nullable=true)
+	 */
+	private $headImage;
+
+	/**
+	 * @return Image
+	 */
+	public function getHeadImage() {
+		return $this->headImage;
+	}
+
+	/**
+	 * @param Image $headImage
+	 *
+	 * @return Post
+	 */
+	public function setHeadImage( Image $headImage ): Post {
+		$this->headImage = $headImage;
+
+		return $this;
+	}
 
 	/**
 	 * @return int
